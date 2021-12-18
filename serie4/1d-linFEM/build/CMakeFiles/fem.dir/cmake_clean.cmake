49a3024ebe92e66ebbc46691440d6a08cd5b1409
apaaris <aparis@ethz.ch>
2021-12-18 15:44:25 +0100

    serie 4

---
+++ serie4/1d-linFEM/build/CMakeFiles/fem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fem.dir/fem.cpp.o"
+  "fem"
+  "fem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
